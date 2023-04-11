643ba24cb0c0fbea79e46df0c8e5657beac220af
MrinalBhoumick <mrinalbhoumick0610@gmail.com>
2023-04-12 01:44:34 +0530

    updated

--- spamDetector.py
+++ spamDetector.py
@@ -14,27 +14,15 @@ def main():
     st.title("Email / SMS Spam Detector")
     st.subheader("Build with Streamlit & Python")
     msg=st.text_area("Enter your Message : ")
-    col1, col2, col3 , col4, col5 = st.beta_columns(5)
-    with col1:
-        pass
-    with col2:
-        pass
-    with col4:
-        pass
-    with col5:
-        pass
-    with col3 :
-        if st.button("Predict"):
-            data=[msg]
-            vect = cv.transform(data).toarray()
-            prediction = model.predict(vect)
-            result = prediction[0]
+    if st.button('Predict'):
+        data=[msg]
+        vect = cv.transform(data).toarray()
+        prediction = model.predict(vect)
+        result = prediction[0]
         if result==1:
             st.error("This is a spam mail")
             speak("This is a spam mail")
         else:
-              st.success("This is not a spam mail")
-              speak("This is not a spam mail")
-
-
+            st.success("This is not a spam mail")
+            speak("This is not a spam mail")
 main()
